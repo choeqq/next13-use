09089ef441f43f634b93f66475b418d338b9efb1
choeqq <k.chola@op.pl>
2022-11-07 23:49:44 +0100

    client side component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-export default async function Home() {
+"use client";
+
+import { use } from "react";
+
+async function fetchData() {
   const res = await fetch("http://localhost:3000/api/hello");
-  const data = await res.json();
+  return res.json();
+}
+
+const dataPromise = fetchData();
+
+export default function Home() {
+  const data = use(dataPromise);
+
   return <div>{JSON.stringify(data, null, 2)}</div>;
 }
