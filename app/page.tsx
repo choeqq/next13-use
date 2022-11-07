2c53cffb56084f8e1980d451e860acbefc7edfe2
choeqq <k.chola@op.pl>
2022-11-08 00:05:58 +0100

    pokemon setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,27 @@
 import { use } from "react";
 
 const fetchMap = new Map<string, Promise<any>>();
-function queryClient(name: string, query: () => Promise<any>) {
+function queryClient<QueryResult>(
+  name: string,
+  query: () => Promise<QueryResult>
+): Promise<QueryResult> {
   if (!fetchMap.has(name)) fetchMap.set(name, query());
 
   return fetchMap.get(name)!;
 }
 
 export default function Home() {
-  const data = use(
-    queryClient("hello", () =>
-      fetch("http://localhost:3000/api/hello").then((res) => res.json())
+  const pokemons = use(
+    queryClient<{ id: number; name: string }[]>("pokemon", () =>
+      fetch("http://localhost:3000/api/pokemon").then((res) => res.json())
     )
   );
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div>
+      {pokemons.map((p) => (
+        <button key={p.id}>{p.name}</button>
+      ))}
+    </div>
+  );
 }
---
+++ pages/api/pokemon.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import pokemon from "../../data/pokemon.json";
+
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<typeof pokemon>
+) {
+  res.status(200).json(pokemon);
+}
